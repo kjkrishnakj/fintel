cf48345a8777d37e538a373e13ebee1f66c915a3
krishna jasiwal <krishnajaswl@gmail.com>
2025-06-24 22:43:01 +0530

    news fix v2

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -17,7 +17,7 @@ export default function Page() {
         } as React.CSSProperties
       }
     >
-      <AppSidebar variant="inset" />
+      <AppSidebar />
       <SidebarInset>
         <SiteHeader />
         <div className="flex flex-1 flex-col">
